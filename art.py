7c538fe3de8af5cc9efa0673456c3aebdff6e47d
Hussain Erfani <145542696+erfani-hussain@users.noreply.github.com>
2024-03-22 16:53:48 +0100

    Add files via upload

---
+++ art.py
@@ -0,0 +1,14 @@
+logo = """
+                                                            
+                         ad88    ad88                       
+                        d8"     d8"                         
+                        88      88                          
+ ,adPPYba,  ,adPPYba, MM88MMM MM88MMM ,adPPYba,  ,adPPYba,  
+a8"     "" a8"     "8a  88      88   a8P_____88 a8P_____88  
+8b         8b       d8  88      88   8PP""""""" 8PP"""""""  
+"8a,   ,aa "8a,   ,a8"  88      88   "8b,   ,aa "8b,   ,aa  
+ `"Ybbd8"'  `"YbbdP"'   88      88    `"Ybbd8"'  `"Ybbd8"'  
+                                                            
+                                                            
+
+"""
